ea7c1ee6f7c5ca67317c98efd1ea8a56daac805a
dawkzor <89469714+dawkk@users.noreply.github.com>
2022-02-21 11:12:00 -0300

    update
    add while condition and try counting, also exit while when guessing right number

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -1,22 +1,31 @@
+from tracemalloc import stop
+
+
 print("***********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("***********************************")
 
 numero_secreto = 43
+total_tentativas = 1
 
-chute_str = input("Digite seu numero: ")
-print("Você digitou ", chute_str)
-chute = int(chute_str)
-acertou = numero_secreto == chute
-chute_maior = numero_secreto > chute
-chute_menor = numero_secreto < chute
+while(total_tentativas < 6):
+    print("Tentativa:", total_tentativas,"de 5")
+    chute_str = input("Digite seu numero: ")
+    # print("Você digitou ", chute_str)
+    chute = int(chute_str)
+    acertou = numero_secreto == chute
+    chute_maior = numero_secreto > chute
+    chute_menor = numero_secreto < chute
 
-if(acertou):
-    print("Vocẽ Acertou!")
-else:
-    if(chute_maior):
-        print("Você Errou! O numero secreto é maior do que", chute_str)
-    elif(chute_menor):
-        print("Você Errou! O numero secreto é menor do que", chute_str)
+    if(acertou):
+        print("Você Acertou!")
+        total_tentativas = 6
+    else:
+        if(chute_maior):
+            print("Você Errou! O numero secreto é maior do que", chute_str)
+        elif(chute_menor):
+            print("Você Errou! O numero secreto é menor do que", chute_str)
+
+    total_tentativas = total_tentativas + 1
 
 print("*************FIM DO JOGO******************")
